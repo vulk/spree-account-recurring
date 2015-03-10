264356ff9ed95a0f7c45bef1b7e40f9b6ec65939
Wolfpack <wolfpack@vulk.co>
2015-03-10 06:37:53 -0400

    logging statements

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
@@ -6,10 +6,11 @@ module Spree
           def subscribe(subscription)
             raise_invalid_object_error(subscription, Spree::Subscription)
             customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
-            puts "subscription handler customer is: #{customer}"
-            unless customer.nil?
+            puts "subcribe() subscription handler customer is: #{customer}"
+            puts "subcribe() subscription is: #{subscription}"
+            # unless customer.nil?
               customer.subscriptions.create(plan: subscription.api_plan_id)
-            end
+            # end
           end
 
           def unsubscribe(subscription)
